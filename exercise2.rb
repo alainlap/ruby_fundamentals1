ee907b637c6427382169fe0763ea0999aa329342
alainlap <alainlaplume@gmail.com>
2013-10-22 10:02:52 -0400

    finished exercise 2 and started exercise 3

--- exercise2.rb
+++ exercise2.rb
@@ -21,3 +21,16 @@ puts false || false
 
 # This will print true
 puts !false || false
+
+# Tip on 5 dollar meal
+puts "If you tip 15% on a $5.00 meal, you will pay a total of #{5*1.15}"
+
+# adding a string and an integer - output as integer
+puts (7+14.2).to_i
+
+# some multiplication
+
+puts "Here's the answer: #{45682*7839}, with interpolation"
+
+# will print true
+puts (true && false) || (false && true) || !(false && false)
