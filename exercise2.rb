1f42684c421dc680e5edd862f0bebbfd96aa94a8
alainlap <alainlaplume@gmail.com>
2013-10-22 09:51:03 -0400

    added exercise2

---
+++ exercise2.rb
@@ -0,0 +1,23 @@
+# This will print true
+puts true && true
+
+# This will print false
+puts true && false
+
+# This will print true
+puts true && !false
+
+# This will print true
+puts true || false
+
+# This will print true
+puts true || true
+
+# This will print true
+puts false || true
+
+# This will print false
+puts false || false
+
+# This will print true
+puts !false || false
